e6f896017d42ec1cacda50c4634526acbc9fbda1
Kristin Valido <kristinvalido@outlook.com>
2026-03-03 18:39:16 -0800

    Update voteSystem.js
    added in user vote to prevent multiple votes

--- react-frontend/src/voteSystem.js
+++ react-frontend/src/voteSystem.js
@@ -1,5 +1,5 @@
 import { db } from "./firebase"; //athen /confic 
-import {doc, updateDoc, increment, getDoc} from "firebase/firestore";
+import {doc, updateDoc, increment, getDoc, setDoc} from "firebase/firestore";
 
 
 const currentSongRef = doc(db, "playback", "current");
@@ -9,7 +9,8 @@ export const upvoteSong = async () => {
     await updateDoc(currentSongRef, {
       upvotes: increment(1)
     });
-  } catch (error) {
+  } 
+  catch (error) {
     console.error("Upvote failed:", error);
   }
 };
@@ -20,7 +21,8 @@ export const downvoteSong = async () => {
     await updateDoc(currentSongRef, {
       downvotes: increment(1)
     });
-  } catch (error) {
+  } 
+  catch (error) {
     console.error("Downvote failed:", error);
   }
 };
@@ -28,11 +30,9 @@ export const downvoteSong = async () => {
 //reset votes 
 export const resetVotes = async () => {
   try {
-    await updateDoc(currentSongRef, {
-      upvotes: 0,
-      downvotes: 0
-    });
-  } catch (error) {
+    await updateDoc(currentSongRef, {upvotes: 0, downvotes: 0});
+  } 
+  catch (error) {
     console.error("Reset failed:", error);
   }
 };
@@ -43,21 +43,49 @@ export const getVoteData = async () => {
     const snapshot = await getDoc(currentSongRef);
     if (snapshot.exists()) {
       const data = snapshot.data();
-      return {
-        upvotes: data.upvotes || 0,
-        downvotes: data.downvotes || 0
-      };
+      return { upvotes: data.upvotes || 0, downvotes: data.downvotes || 0};
     }
-  } catch (error) {
+  } 
+  catch (error) {
     console.error("Get votes failed:", error);
   }
 
   return { upvotes: 0, downvotes: 0 };
+
+  //need to limit miltple voting, tracked with Id
+  explort const trackUserVoting = async (userId, voteType) => {
+    try {
+      const voteRef = doc(db, "votes", `${userId}_current`);
+      const existingVoter = await getDoc(voteRef);
+
+      if (exsitingVoter.exists()) {
+        console.log("You already votes.");
+        return
+      }
+      await setDoc(voteRef, {userId, voteType, timestamp: new Date()}); // saves users vote
+
+      if (voteType == "up") {
+        await updateDoc(currentSongRef, {
+          upvotes : increment(1)
+        });
+      } 
+      else {
+        await updateDoc(currentSongRef, {
+          downvotes: increment(1)
+        });
+      }
+    }
+
+    catch (error){
+      console.error("Vote failed:", error);
+    }
+      
+  
 };
 
   //TODO
   // add upvote and downvote button 
-  //need to add it to display
-  // add in prevent multiple users voting 
-  //
+  //need to add it to NowPlaying.jsx
+  // need to connect it to spotify api
+
 
